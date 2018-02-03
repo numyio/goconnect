d19a4db6637a602a9ba09515ff80dff916c0eefc
Joshua Hester <jhester@numy.io>
2018-02-03 10:44:06 -0600

    merge conflict changes

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // User : for input form
@@ -22,7 +25,6 @@ func main() {
 
 func echoServer() {
 	e := echo.New()
-<<<<<<< HEAD
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Static("/", "/static")
@@ -40,20 +42,6 @@ func httpServer() {
 	router.HandleFunc("/accounts/{id}", createAccount).Methods("POST")
 	router.HandleFunc("/accounts/{id}", deleteAccount).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
-=======
-	e.Static("/", "static")
-	e.GET("/users/:id", getUser)
-	e.POST("/users/:id", newUser)
-
-	// For websockets in the future
-	// fmt.Println("Listening on port 8000 for REST requests")
-	// router := mux.NewRouter()
-	// router.HandleFunc("/accounts", getAccounts).Methods("GET")
-	// router.HandleFunc("/accounts/{id}", getAccount).Methods("GET")
-	// router.HandleFunc("/accounts/{id}", createAccount).Methods("POST")
-	// router.HandleFunc("/accounts/{id}", deleteAccount).Methods("DELETE")
-	// log.Fatal(http.ListenAndServe(":8000", router))
-	e.Logger.Fatal(e.Start(":1323"))
 }
 
 // e.GET("/users/:id", getUser)
@@ -77,5 +65,4 @@ func newUser(c echo.Context) error {
 	fmt.Println("Inserting Password: " + u.Password)
 	fmt.Println("Inserting Email: " + u.Email)
 	return result
->>>>>>> c17f779d5de7d01342407d3d05f77070c7b8f4de
 }
